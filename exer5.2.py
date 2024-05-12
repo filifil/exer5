e03a80b3811be2d9d4a3ed66584ef9cb08b16036
filifil <filifilibro@gmail.com>
2024-05-12 16:23:34 +0200

    solution for exer

---
+++ exer5.2.py
@@ -0,0 +1,11 @@
+list1 =[33,44,22,12,55]
+aver1 = sum(list1)/len(list1)
+print('The average was ',aver1)
+
+list1.append(10)
+print('Added 10, new list is',list1)
+
+list2 = [33,44,22,12,55,10]
+aver2=sum(list2)//len(list2)
+print('The average is now',aver2)
+
